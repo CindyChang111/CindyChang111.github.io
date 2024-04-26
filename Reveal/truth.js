c10a16ef721d8549d16a7cad1baaf3bda2087731
CindyChang111 <142123325+CindyChang111@users.noreply.github.com>
2024-04-26 19:35:54 -0400

    Add files via upload

---
+++ Reveal/truth.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var font = 'sans-serif';
+var letter = 'Truth';
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  fill(0);
+
+  textFont(font);
+  textAlign(CENTER, CENTER);
+}
+
+function mouseMoved() {
+  clear();
+  textSize((mouseX - width / 2) * 5 + 1);
+  text(letter, width / 2, mouseY);
+}
+
+function mouseDragged() {
+  textSize((mouseX - width / 2) * 5 + 1);
+  text(letter, width / 2, mouseY);
+}
+
+function keyReleased() {
+  if (keyCode == CONTROL) saveCanvas(gd.timestamp(), 'png');
+}
+
+function keyTyped() {
+  letter = key;
+}
